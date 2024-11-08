3e5d5870d9e56a873783db83fc96385b1e01d745
Tiago-Garcia <49250332+Tiago-Garcia@users.noreply.github.com>
2024-11-08 10:42:12 -0300

    Create cypress project

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -2,6 +2,6 @@
 it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function () {
     cy.visit('./src/privacy.html')
 
-    cy.contains('Talking About Tsting').should('be.visible')
+    cy.contains('Talking About Testing').should('be.visible')
 
 })
